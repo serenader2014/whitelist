7fffbf7c1fbefadcb91f502a2818fb774b7a2cf7
serenader <xyslive@gmail.com>
2014-07-23 11:42:03 +0800

    handle classes request

--- client/routes/index.js
+++ client/routes/index.js
@@ -20,7 +20,7 @@ router.get('/', function(req, res) {
 
 router.get('/:class', function (req, res) {
     var className = req.params.class;
-    var request = http.request(target+className, function (response) {
+    var request = http.request(target+'/'+className, function (response) {
         response.on('data', function (chunk) {
             var data = JSON.parse(chunk);
             var requestClass = http.request(target, function (responseClass) {
@@ -29,8 +29,32 @@ router.get('/:class', function (req, res) {
                     res.render('class', {classes: classes, product: data});
                 });
             });
+
+            requestClass.end();
         });
     });
+
+    request.end();
+});
+
+router.get('/:class/:product', function (req, res) {
+    var className = req.params.class;
+    var productName = req.params.productName;
+    var requestProduct = http.request(target+'/'+className+'/'+productName, function (responseProduct) {
+        responseProduct.on('data', function (rawProduct) {
+            var products = JSON.parse(rawProduct);
+            var requestSiblings = http.request(target+'/'+className, function (responseSiblings) {
+                responseSiblings.on('data', function (rawSibling) {
+                    var siblings = JSON.parse(rawSibling);
+                    res.render('product', {siblings: siblings, products: products});
+                });
+            });
+
+            requestSiblings.end();
+        });
+    });
+
+    requestProduct.end();
 });
 
 module.exports = router;
